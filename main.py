d81d3db1f889a1a909e4a3ed0960a934abeaa689
RAYABARAPU VARUN RAJ <varuncraj.r@gmail.com>
2025-07-10 21:26:19 +0530

    vercel update

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from mangum import Mangum
 from fastapi.middleware.cors import CORSMiddleware
 from src.routes import ask_Q
 
@@ -9,3 +10,5 @@ app.include_router(ask_Q.router, prefix="/Q", tags=["ask Question"])
 @app.get("/")
 def homepage():
     return "lexi backend test home page"
+
+handler = Mangum(app)
