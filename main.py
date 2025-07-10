de53199cb71c41495d4c4ed7ff257e10c43b6d33
RAYABARAPU VARUN RAJ <varuncraj.r@gmail.com>
2025-07-10 21:58:07 +0530

    render update1

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
 from fastapi import FastAPI
-from mangum import Mangum
-from fastapi.middleware.cors import CORSMiddleware
 from src.routes import ask_Q
 
 app = FastAPI()
@@ -11,4 +9,3 @@ app.include_router(ask_Q.router, prefix="/Q", tags=["ask Question"])
 def homepage():
     return "lexi backend test home page"
 
-handler = Mangum(app)
